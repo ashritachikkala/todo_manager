1ebb5ec19644372e0aab39a5c94f279112713b04
ashritachikkala <ashritachikkala@gmail.com>
2021-06-06 00:36:39 +0530

    migrated database to create user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   #get "todos", to: "todos#index"
   #post "todos/create", to: "todos#create"
   #get "todos/:id", to: "todos#show"
   
   resources :todos
+  resources :users
 end
