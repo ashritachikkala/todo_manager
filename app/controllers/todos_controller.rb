41dcd2712105267ae31f5cd8d8016e261f99acac
ashritachikkala <ashritachikkala@gmail.com>
2021-06-05 16:34:35 +0530

    Used resources routing convention

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def index
         render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string}.join("\n")
     end
@@ -6,5 +7,12 @@ class TodosController < ApplicationController
         id = params[:id]
         todo = Todo.find(id)
         render plain: todo.to_pleasant_string
+    end
+    def create
+        todo_text = params[:todo_text]
+        due_date = DateTime.parse(params[:due_date]) #parsing the due date as format of datetime
+        new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+        response_text = "Hey your new todo is created with the id: #{new_todo.id}"
+        render plain: response_text
     end
 end
