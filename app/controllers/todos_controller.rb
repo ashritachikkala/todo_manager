a57b1d4da6c8a093f8e767881ca8a040e0f28293
ashritachikkala <ashritachikkala@gmail.com>
2021-06-29 15:51:54 +0530

    add,delete,mark todo(with rails form helpers)

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,8 +1,6 @@
 class TodosController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
+  
   def index
-    #render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
   end
 
@@ -16,8 +14,7 @@ class TodosController < ApplicationController
     todo_text = params[:todo_text]
     due_date = DateTime.parse(params[:due_date]) #parsing the due date as format of datetime
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    response_text = "Hey your new todo is created with the id: #{new_todo.id}"
-    render plain: response_text
+    redirect_to todos_path
   end
 
   def update
@@ -26,6 +23,13 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "Updated todo complete status to #{completed}"
+    redirect_to todos_path
+  end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
   end
 end
